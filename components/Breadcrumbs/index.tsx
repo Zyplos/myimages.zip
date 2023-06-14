afd6123cecb3cb302f1b347e5d1a300b9c67ed2b
Angel <22824872+Zyplos@users.noreply.github.com>
2023-06-13 20:00:49 -0500

    made icons

--- components/Breadcrumbs/index.tsx
+++ components/Breadcrumbs/index.tsx
@@ -2,31 +2,43 @@
 
 import styles from "./Breadcrumbs.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Breadcrumbs() {
   const pathname = usePathname();
-  const items = ["C:\\Users\\zyplos\\Downloads\\myimages.zip\\"];
+  console.log("BREADCRUMBS CURRENT pathname", pathname);
+
+  const items = ["C:\\Users\\offic\\Downloads\\myimages.zip\\"];
   pathname.split("/").forEach((item) => {
     if (item !== "") {
       items.push(item + "\\");
     }
   });
+  // for each item, create a link that goes up one level
+  const Links = items.map((item, index) => {
+    const upLevelLink = pathname.split("/").slice(1).slice(0, index).join("/");
+    return (
+      <Link key={index} href={"/" + upLevelLink}>
+        {item}
+      </Link>
+    );
+  });
+
+  // remove last path segment to go up one level
+  const upLevelLink = pathname.split("/").slice(1).slice(0, -1).join("/");
+  const isIndex = pathname == "/";
 
   return (
     <div className={styles["breadcrumbs-wrapper"]}>
       <div className={styles["up-button-wrapper"]}>
-        <button className={styles["up-button"]}>
-          <Image src="/images/icons/test-icon.svg" width="16" height="16" alt="Test Icon" />
-        </button>
+        <Link className={styles["up-button"]} href={"/" + upLevelLink}>
+          <Image src="/images/icons/levelup-icon.svg" width="18" height="18" alt="Test Icon" />
+        </Link>
       </div>
       <div className={styles["breadcrumbs-list-wrapper"]}>
-        <Image src="/images/icons/test-icon.svg" width="18" height="18" alt="Test Icon" />
-        <div className={styles["breadcrumbs-list"]}>
-          {items.map((item, index) => (
-            <button key={index}>{item}</button>
-          ))}
-        </div>
+        <Image src={`/images/icons/${isIndex ? "zipfile" : "folder"}-icon.svg`} width="18" height="18" alt="Test Icon" />
+        <div className={styles["breadcrumbs-list"]}>{Links}</div>
       </div>
     </div>
   );
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className={styles["navbar-wrapper"]}>
       <div className={styles.toolbar}>
         <button>File</button>
         <button>Edit</button>
@@ -19,11 +19,11 @@ export default function Navbar() {
         </div>
         <div className={styles["icon-toolbar"]}>
           <button>
-            <Image src="/images/icons/test-icon.svg" width="48" height="48" alt="Test Icon" />
+            <Image src="/images/icons/add-icon.svg" width="48" height="48" alt="Test Icon" />
             <span>Add</span>
           </button>
           <button>
-            <Image src="/images/icons/test-icon.svg" width="48" height="48" alt="Test Icon" />
+            <Image src="/images/icons/extract-icon.svg" width="48" height="48" alt="Test Icon" />
             <span>Extract</span>
           </button>
           <button>
@@ -31,19 +31,19 @@ export default function Navbar() {
             <span>Test</span>
           </button>
           <button>
-            <Image src="/images/icons/test-icon.svg" width="48" height="48" alt="Test Icon" />
+            <Image src="/images/icons/copy-icon.svg" width="48" height="48" alt="Test Icon" />
             <span>Copy</span>
           </button>
           <button>
-            <Image src="/images/icons/test-icon.svg" width="48" height="48" alt="Test Icon" />
+            <Image src="/images/icons/move-icon.svg" width="48" height="48" alt="Test Icon" />
             <span>Move</span>
           </button>
           <button>
-            <Image src="/images/icons/test-icon.svg" width="48" height="48" alt="Test Icon" />
+            <Image src="/images/icons/delete-icon.svg" width="48" height="48" alt="Test Icon" />
             <span>Delete</span>
           </button>
           <button>
-            <Image src="/images/icons/test-icon.svg" width="48" height="48" alt="Test Icon" />
+            <Image src="/images/icons/info-icon.svg" width="48" height="48" alt="Test Icon" />
             <span>Info</span>
           </button>
         </div>
