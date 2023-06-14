2c0cc8777a5e2c1f42dfe639f4d3e679c6a09c3d
Angel <22824872+Zyplos@users.noreply.github.com>
2023-06-13 20:01:10 -0500

    test root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { getDirectoryContents } from "@/internals/utils";
 
 export default function Home() {
+  const rootcontents = getDirectoryContents("");
+  // console.log(rootcontents);
   return (
     <div>
-      <Link href="/mingus">quake3-latest-pk3s | folder | etc</Link>
+      {/* <Link href="/mingus">quake3-latest-pk3s | folder | etc</Link> */}
+      {rootcontents.map((item) => {
+        if (item.type === "directory") {
+          return (
+            <div key={item.name} style={{ backgroundColor: "green" }}>
+              <Link href={`/${item.name}`}>FOLDER: {item.name}</Link>
+            </div>
+          );
+        } else {
+          return (
+            <div key={item.name}>
+              <a href={`/${item.name}.${item.type}`}>
+                {item.name}.{item.type}
+              </a>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
