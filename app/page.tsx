f3598db02a59bfa8b5b0615ab4ffbf7ba1f4f780
Angel <22824872+Zyplos@users.noreply.github.com>
2023-06-15 09:47:33 -0500

    move table into component, root page fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
-import { getDirectoryContents } from "@/internals/utils";
+import FileExplorer from "@/components/FileExplorer";
 
 export default function Home() {
-  const rootcontents = getDirectoryContents("");
-  // console.log(rootcontents);
-  return (
-    <div>
-      {/* <Link href="/mingus">quake3-latest-pk3s | folder | etc</Link> */}
-      {rootcontents.map((item) => {
-        if (item.type === "directory") {
-          return (
-            <div key={item.name} style={{ backgroundColor: "green" }}>
-              <Link href={`/${item.name}`}>FOLDER: {item.name}</Link>
-            </div>
-          );
-        } else {
-          return (
-            <div key={item.name}>
-              <a href={`/${item.name}.${item.type}`}>
-                {item.name}.{item.type}
-              </a>
-            </div>
-          );
-        }
-      })}
-    </div>
-  );
+  return <FileExplorer />;
 }
---
+++ components/FileExplorer/index.tsx
@@ -0,0 +1,320 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./page.module.scss";
+import { usePathname } from "next/navigation";
+import { ItemData, convertDateFormat, formatIntegerWithSpaces, getDirectoryContents } from "@/internals/utils";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Loader from "@/components/Loader";
+
+interface Column {
+  header: HTMLTableCellElement;
+  size: string;
+}
+
+interface ColumnSizeData {
+  header: string;
+  size: string;
+  type: "numeric" | "text-short" | "text-long";
+}
+
+const min = 150;
+const columnTypeToRatioMap: { [key: string]: number } = {
+  numeric: 1,
+  "text-short": 1.67,
+  "text-long": 3.33,
+};
+
+// given an element, find the parent td element and then return the contents of the 2nd td child
+function grabBytesDataFromDOM(span: HTMLSpanElement) {
+  const td = span.parentElement?.parentElement?.parentElement?.children[1];
+  if (!td) return null;
+  return td.textContent;
+}
+// same thing as grabBytesDataFromDOM but returns the modified date instead
+function grabModifiedDateFromDOM(span: HTMLSpanElement) {
+  const td = span.parentElement?.parentElement?.parentElement?.children[2];
+  if (!td) return null;
+  return td.textContent;
+}
+
+// [ '.jpg', '.png', '.url', '.gif', '.jpeg', '.JPG' ]
+const typeToIconMap: { [key: string]: string } = {
+  directory: "folder",
+  jpg: "jpg",
+  png: "png",
+  url: "url",
+  gif: "gif",
+  jpeg: "jpg",
+  JPG: "jpg",
+};
+
+function TypedIcon({ type }: { type: string }) {
+  return <Image src={`/images/icons/${typeToIconMap[type] || "defaultfile"}-icon.svg`} width="16" height="16" alt="Test Icon" />;
+}
+
+function CorrectLink({ item, currentPath }: { item: ItemData; currentPath: string }) {
+  const router = useRouter();
+  if (item.type === "directory") {
+    return (
+      <>
+        <button
+          onDoubleClick={() => router.push(currentPath + `/${item.name}`)}
+          onKeyUp={(e) => {
+            // if key is enter or numpad enter
+            if (e.key === "Enter" || e.key === "NumpadEnter") {
+              router.push(currentPath + `/${item.name}`);
+            }
+          }}
+        >
+          <TypedIcon type={item.type} />
+          <span>{item.name}</span>
+        </button>
+      </>
+    );
+  } else if (item.type === "url") {
+    return (
+      <>
+        <button
+          onDoubleClick={() => window.open(item.url)}
+          onKeyUp={(e) => {
+            // if key is enter or numpad enter
+            if (e.key === "Enter" || e.key === "NumpadEnter") {
+              window.open(item.url);
+            }
+          }}
+        >
+          <TypedIcon type={item.type} />
+          <span>
+            {item.name}.{item.type}
+          </span>
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <button
+          onDoubleClick={() => window.open(`/images/_filespace${currentPath}/${item.name}.${item.type}`)}
+          onKeyUp={(e) => {
+            // if key is enter or numpad enter
+            if (e.key === "Enter" || e.key === "NumpadEnter") {
+              window.open(`/images/_filespace${currentPath}/${item.name}.${item.type}`);
+            }
+          }}
+        >
+          <TypedIcon type={item.type} />
+          <span>
+            {item.name}.{item.type}
+          </span>
+        </button>
+      </>
+    );
+  }
+}
+
+function getColumnSizesFromLocalStorage(): string[] {
+  try {
+    return JSON.parse(localStorage.getItem("columns") || "[]");
+  } catch (error) {
+    console.error("couldnt parse localstorage", error);
+    return [];
+  }
+}
+
+export default function FileExplorer() {
+  const [currentlySelected, setCurrentlySelected] = useState<HTMLButtonElement | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // https://adamlynch.com/flexible-data-tables-with-css-grid/
+    const table = document.querySelector("table");
+    if (!table) return;
+
+    let localColumnData = getColumnSizesFromLocalStorage();
+
+    const columns: Column[] = [];
+    let headerBeingResized: HTMLElement | null;
+
+    const onMouseMove = (e: MouseEvent) =>
+      requestAnimationFrame(() => {
+        console.log("onMouseMove");
+
+        if (!headerBeingResized) return;
+
+        const horizontalScrollOffset = document.documentElement.scrollLeft;
+        const width = horizontalScrollOffset + e.clientX - headerBeingResized!.offsetLeft;
+
+        const column = columns.find(({ header }) => header === headerBeingResized);
+        if (column) {
+          column.size = Math.max(min, width) + "px";
+        }
+        console.log("]]]]]]]]]]COLUMNS CHANGED", columns);
+        try {
+          // save columns data to localstorage, but only the size property
+          localStorage.setItem("columns", JSON.stringify(columns.map(({ size }) => size)));
+          console.log(
+            "saved to localstorage",
+            columns.map(({ size }) => size)
+          );
+        } catch (error) {
+          console.error("couldnt save to localstorage", error);
+        }
+
+        const CONSTRUCTED_STYLE_STRING = columns.map(({ size }) => size).join(" ");
+        console.log("CONSTRUCTED_STYLE_STRING", CONSTRUCTED_STYLE_STRING);
+        table.style.gridTemplateColumns = CONSTRUCTED_STYLE_STRING;
+      });
+
+    const onMouseUp = () => {
+      console.log("onMouseUp");
+
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mouseup", onMouseUp);
+      if (headerBeingResized) {
+        headerBeingResized.classList.remove("header--being-resized");
+      }
+      headerBeingResized = null;
+    };
+
+    const initResize = ({ target }: MouseEvent) => {
+      console.log("initResize");
+
+      headerBeingResized = (target as HTMLSpanElement).parentNode as HTMLElement;
+      window.addEventListener("mousemove", onMouseMove);
+      window.addEventListener("mouseup", onMouseUp);
+      if (headerBeingResized) {
+        headerBeingResized.classList.add("header--being-resized");
+      }
+    };
+
+    // GRAB COLUMNS FROM DOM
+    document.querySelectorAll("th").forEach((header, index) => {
+      console.log("==========header", header, index);
+      const max = columnTypeToRatioMap[header.dataset.type as string] + "fr";
+      columns.push({
+        header,
+        size: localColumnData[index] || `minmax(${min}px, ${max})`,
+      });
+      console.log("==========columns", columns);
+      table.style.gridTemplateColumns = columns.map(({ size }) => size).join(" ");
+      setIsLoading(false);
+
+      const resizeHandle = header.querySelector(".resize-handle") as HTMLElement;
+      resizeHandle.addEventListener("mousedown", initResize);
+    });
+
+    // ===== SET LAST FOCUSED LINK
+    const setLastFocusedLink = (e: Event) => {
+      const target = e.target as HTMLButtonElement;
+      // const button = target.closest("button");
+      // console.log("SETTING LAST FOCUSED LINK", button);
+
+      // remove "active-border" from all td a elements
+      document.querySelectorAll("td button span").forEach((link) => {
+        link.classList.remove(styles["active-border"]);
+      });
+
+      // add "active-border" to the clicked td a element
+      target.classList.add(styles["active-border"]);
+
+      setCurrentlySelected(target);
+    };
+    document.querySelectorAll("td button span").forEach((link) => {
+      link.addEventListener("mousedown", setLastFocusedLink);
+    });
+
+    // Clean up event listeners in the return function
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mouseup", onMouseUp);
+      columns.forEach(({ header }) => {
+        const resizeHandle = header.querySelector(".resize-handle") as HTMLElement;
+        resizeHandle.removeEventListener("mousedown", initResize);
+      });
+
+      document.querySelectorAll("td button span").forEach((link) => {
+        link.removeEventListener("mousedown", setLastFocusedLink);
+      });
+    };
+  }, []);
+
+  const pathname = usePathname();
+  console.log("CURRENT PATHNAME: ", pathname);
+  // remove beginning slash
+  const pathFixed = pathname.substring(1);
+
+  const rootcontents = getDirectoryContents(pathFixed);
+  console.log(rootcontents);
+
+  // sort by directory first, then by name
+  rootcontents.sort((a, b) => {
+    if (a.type === "directory" && b.type !== "directory") {
+      return -1;
+
+      // if a is not a directory and b is a directory, return 1
+    } else if (a.type !== "directory" && b.type === "directory") {
+      return 1;
+
+      // if a and b are both directories, sort by name
+    } else if (a.type === "directory" && b.type === "directory") {
+      return a.name.localeCompare(b.name);
+
+      // if a and b are both not directories, sort by name
+    } else {
+      return a.name.localeCompare(b.name);
+    }
+  });
+
+  console.log("IS LOADING: ", isLoading);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <table
+        className={`${styles.table} table`}
+        style={{
+          ...(isLoading && { display: "none" }),
+        }}
+      >
+        <thead>
+          <tr>
+            <th data-type="text-long">
+              Name <span className={`resize-handle ${styles["resize-handle"]}`}></span>
+            </th>
+            <th data-type="numeric">
+              Size <span className={`resize-handle ${styles["resize-handle"]}`}></span>
+            </th>
+            <th data-type="text-short">
+              Modified <span className={`resize-handle ${styles["resize-handle"]}`}></span>
+            </th>
+            <th data-type="text-short">
+              Created <span className={`resize-handle ${styles["resize-handle"]}`}></span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {rootcontents.map((item, index) => (
+            <tr key={index}>
+              <td>
+                <CorrectLink item={item} currentPath={pathname} />
+              </td>
+              <td>{item.type == "directory" ? "" : formatIntegerWithSpaces(item.size)}</td>
+              <td>{convertDateFormat(item.modified)}</td>
+              <td>{convertDateFormat(item.created)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <footer className={styles["footer-toolbar"]}>
+        <span>
+          {currentlySelected ? 1 : 0} / {rootcontents.length} object(s) selected
+        </span>
+        <span>{currentlySelected ? grabBytesDataFromDOM(currentlySelected) : ""}</span>
+        <span>{currentlySelected ? grabBytesDataFromDOM(currentlySelected) : ""}</span>
+        <span>{currentlySelected ? grabModifiedDateFromDOM(currentlySelected) : ""}</span>
+      </footer>
+    </>
+  );
+}
